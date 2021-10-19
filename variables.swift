c0a017ad84fda4f3ad10e406c0066cd858730b79
mmjck <msantosjck@gmail.com>
2021-10-19 13:08:07 -0400

    variables

---
+++ variables.swift
@@ -0,0 +1,27 @@
+let apples = 3
+let oranges = 2
+
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit." 
+
+print(appleSummary)
+print(fruitSummary)
+
+
+var myList = ["car", "mouse", "water"]
+
+print(myList)
+print("Element index 0", myList[0])
+
+var person = [
+    "name": "Jack",
+    "Job": "Public Relations",
+]
+
+print(person)
+
+
+func sum(a: Int, b: Int) -> Int {
+    return a + b
+}
+print(sum(a:1, b:2))
