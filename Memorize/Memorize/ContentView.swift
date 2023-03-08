1968f7372a8e1f16a3f4e7d0094620890367a74e
mmjck <msantosjck@gmail.com>
2023-03-07 22:26:34 -0400

    second project

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Jackson Matheus on 07/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jackson Matheus on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
