397cd514372f31d8c3c8848064a9c2eeebd822c0
mmjck <msantosjck@gmail.com>
2024-01-05 14:02:44 -0400

    add

--- concurrency.playground/Contents.swift
+++ concurrency.playground/Contents.swift
@@ -27,3 +27,20 @@ concurrentQueue.async {
     print("Task 2 started")
     print("Task 2 finished")
 }
+
+
+let operationQueue = OperationQueue()
+
+
+let operation1 = BlockOperation {
+    print("Operation 1")
+}
+
+let operation2 = BlockOperation {
+    print("Operation 2")
+}
+
+operation1.addDependency(operation2)
+
+
+operationQueue.addOperations([operation1, operation2], waitUntilFinished: false)
